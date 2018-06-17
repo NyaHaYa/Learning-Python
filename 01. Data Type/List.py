a9b9f537b7bfb52f73adc9727822b7bb4725e935
Seok Min <gameplay3542@icloud.com>
2018-06-17 23:35:29 +0900

    ADD : 01. Data Type - 3. List

---
+++ 01. Data Type/List.py
@@ -0,0 +1,29 @@
+# 자료형
+
+## 리스트 자료형
+
+
+list = [ 1,2,3,4,5 ]
+print(list)             # [1,2,3,4,5]
+
+list = []
+
+list1 = [1,2,3,]
+
+list2 = ['Life', 'is', 'Egg']
+
+list3 = [1, 2, 'Life', 'is']
+
+list4 = [1, 2, ['Life', 'is']]
+
+## 리스트 인덱싱
+list = [1,2,3]
+print(list)             # [1,2,3]
+
+print(list[0])          # 1
+
+list[0] + list[2]       # 4
+
+list[-1]                # 3
+
+list = [1,2,3,['a','b','c']]
