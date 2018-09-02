3740f560ada407469eacd37a55abfec8a70b128d
Y0ON <gameplay3542@icloud.com>
2018-09-02 23:56:31 +0900

    [Add] : Practice Problem Folder

---
+++ 01. Data Type/PracticeProblem/3.List.py
@@ -0,0 +1,3 @@
+# 1. 리스트 인덱싱
+a = ['Life', 'is', 'too', 'short', 'you', 'need', 'python']             # -> you too
+print(a[4]+' '+a[2])                                                    # you toos
